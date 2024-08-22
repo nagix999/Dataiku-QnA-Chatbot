d1b6ef40343a2447531e792f9d3b882a8592a5b4
gnkim827 <nagix999@gmail.com>
2024-08-22 23:30:04 +0900

    Update database.py

--- app/backend/settings/database.py
+++ app/backend/settings/database.py
@@ -1,12 +1,13 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+from models import VectorStore
 
-load_dotenv(os.environ["envfile"], verbose=True)
+
+load_dotenv(".env", verbose=True)
 
 def get_db():
     db = SessionLocal()
@@ -19,11 +20,11 @@ def get_db_not_yield():
     db = SessionLocal()
     return db
 
-url = os.environ["POSTGRESQL_CONNECTION_STRING"]
-dbschema = "llmops,public"
+url = os.environ["POSTGRES_CONNECTION_STRING"]
+dbschema = "postgres,public"
 
 engine = create_engine(url, connect_args={'options': '-csearch_path={}'.format(dbschema)})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
 
